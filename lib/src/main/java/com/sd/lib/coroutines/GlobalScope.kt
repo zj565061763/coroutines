7857ad6ccaeca6636d3df7c9b48a5a53279faa5f
zjun <565061763@qq.com>
2024-11-21 04:26:54 +0800

    FGlobalScope

--- lib/src/main/java/com/sd/lib/coroutines/GlobalScope.kt
+++ lib/src/main/java/com/sd/lib/coroutines/GlobalScope.kt
@@ -1,14 +1,16 @@
 package com.sd.lib.coroutines
 
+import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
 import kotlinx.coroutines.supervisorScope
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
-private val FGlobalScope = MainScope()
+private val FGlobalScope: CoroutineScope = MainScope() + CoroutineName("FGlobalScope")
 
 /**
  * 启动全局协程，[block]总是在主线程按顺序执行
