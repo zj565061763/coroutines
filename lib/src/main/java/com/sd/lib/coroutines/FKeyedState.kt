8b740a1687b7cdc17b3edd51b874d08b5e9d95bb
zjun <565061763@qq.com>
2024-11-22 01:16:35 +0800

    Refactor FKeyedState

--- lib/src/main/java/com/sd/lib/coroutines/FKeyedState.kt
+++ lib/src/main/java/com/sd/lib/coroutines/FKeyedState.kt
@@ -1,6 +1,7 @@
 package com.sd.lib.coroutines
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.channelFlow
@@ -12,48 +13,37 @@ class FKeyedState<T>(
 ) {
    private val _holder: MutableMap<String, KeyedFlow> = mutableMapOf()
 
+   /** 获取[key]对应的状态流 */
+   fun flowOf(key: String): Flow<T> {
+      return channelFlow {
+         withContext(Dispatchers.fPreferMainImmediate) {
+            _holder.getOrPut(key) { KeyedFlow(key, getDefault(key)) }
+         }.also { flow ->
+            flow.collect { data ->
+               send(data)
+            }
+         }
+      }
+   }
+
    /** 更新[key]对应的状态 */
    fun update(key: String, state: T) {
-      updateInternal(
-         key = key,
-         state = state,
-         release = false
-      )
+      updateInternal(key = key, state = state, release = false)
    }
 
    /** 更新[key]对应的状态，并在更新之后尝试释放该状态 */
    fun updateAndRelease(key: String, state: T) {
-      updateInternal(
-         key = key,
-         state = state,
-         release = true
-      )
-   }
-
-   /** 获取[key]对应的状态流 */
-   fun flowOf(key: String): Flow<T> {
-      return channelFlow {
-         collect(key) {
-            send(it)
-         }
-      }
+      updateInternal(key = key, state = state, release = true)
    }
 
    private fun updateInternal(key: String, state: T, release: Boolean) {
       /** 注意，这里要切换到[Dispatchers.Main]保证按调用顺序更新状态 */
       fGlobalLaunch(Dispatchers.Main) {
-         val holder = _holder.getOrPut(key) { KeyedFlow(key, state) }
-         holder.update(state)
+         val flow = _holder.getOrPut(key) { KeyedFlow(key, state) }
+         flow.update(state)
          if (release) {
-            holder.release()
+            flow.release()
          }
-      }
-   }
-
-   private suspend fun collect(key: String, block: suspend (T) -> Unit) {
-      withContext(Dispatchers.fPreferMainImmediate) {
-         val holder = _holder.getOrPut(key) { KeyedFlow(key, getDefault(key)) }
-         holder.collect(block)
       }
    }
 
@@ -64,30 +54,34 @@ class FKeyedState<T>(
       private var _releaseAble = true
       private val _flow = MutableStateFlow(initialState)
 
-      fun update(value: T) {
-         _releaseAble = false
-         _flow.value = value
-      }
-
       suspend fun collect(block: suspend (T) -> Unit) {
          try {
             _flow.collect {
                block(it)
             }
          } finally {
-            releaseIfIdle()
+            withContext(NonCancellable) {
+               releaseIfIdle()
+            }
          }
       }
 
-      fun release() {
+      fun update(state: T) {
+         _releaseAble = false
+         _flow.value = state
+      }
+
+      suspend fun release() {
          _releaseAble = true
          releaseIfIdle()
       }
 
-      private fun releaseIfIdle() {
-         if (_releaseAble && _flow.subscriptionCount.value == 0) {
-            _holder.remove(key).also {
-               check(it === this@KeyedFlow)
+      private suspend fun releaseIfIdle() {
+         withContext(Dispatchers.fPreferMainImmediate) {
+            if (_releaseAble && _flow.subscriptionCount.value == 0) {
+               _holder.remove(key).also {
+                  check(it === this@KeyedFlow)
+               }
             }
          }
       }
