dc5be1de842302502797045f8c3f84af226f7a04
zjun <565061763@qq.com>
2024-11-21 13:21:41 +0800

    fGlobalLaunch return job

--- lib/src/main/java/com/sd/lib/coroutines/GlobalScope.kt
+++ lib/src/main/java/com/sd/lib/coroutines/GlobalScope.kt
@@ -3,6 +3,7 @@ package com.sd.lib.coroutines
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.plus
@@ -18,8 +19,8 @@ fun fGlobalLaunch(
    context: CoroutineContext = EmptyCoroutineContext,
    start: CoroutineStart = CoroutineStart.DEFAULT,
    block: suspend CoroutineScope.() -> Unit,
-) {
-   FGlobalScope.launch(
+): Job {
+   return FGlobalScope.launch(
       context = context,
       start = start,
       block = block,
