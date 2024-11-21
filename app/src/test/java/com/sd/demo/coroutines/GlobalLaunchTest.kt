232ac1d79e8878f42d446bba769068b37ff1266a
zjun <565061763@qq.com>
2024-11-21 11:17:11 +0800

    test

--- app/src/test/java/com/sd/demo/coroutines/GlobalLaunchTest.kt
+++ app/src/test/java/com/sd/demo/coroutines/GlobalLaunchTest.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Rule
@@ -25,33 +24,26 @@ class GlobalLaunchTest {
          fGlobalLaunch {
             flow.emit(1)
          }.also {
-            advanceUntilIdle()
             assertEquals(1, awaitItem())
          }
 
          fGlobalLaunch {
             throw CancellationException()
-         }.also {
-            advanceUntilIdle()
          }
 
          fGlobalLaunch {
             flow.emit(2)
          }.also {
-            advanceUntilIdle()
             assertEquals(2, awaitItem())
          }
 
          fGlobalLaunch {
             currentCoroutineContext().cancel()
-         }.also {
-            advanceUntilIdle()
          }
 
          fGlobalLaunch {
             flow.emit(3)
          }.also {
-            advanceUntilIdle()
             assertEquals(3, awaitItem())
          }
       }
