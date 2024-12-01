9ba45d9cd1b8207b776ea98be063401e73a5c433
zjun <565061763@qq.com>
2024-12-01 11:26:58 +0800

    Sample

--- app/src/main/java/com/sd/demo/coroutines/MainActivity.kt
+++ app/src/main/java/com/sd/demo/coroutines/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.sd.demo.coroutines
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.sd.demo.coroutines.databinding.ActivityMainBinding
-import com.sd.lib.coroutines.fGlobalLaunch
-import kotlinx.coroutines.Dispatchers
 
 class MainActivity : AppCompatActivity() {
    private val _binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -13,29 +12,10 @@ class MainActivity : AppCompatActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       setContentView(_binding.root)
-      _binding.btnLaunchGlobalImmediate.setOnClickListener {
-         launchGlobalImmediate()
-      }
-      _binding.btnLaunchGlobalDispatched.setOnClickListener {
-         launchGlobalDispatched()
+      _binding.btnSampleGlobalLaunch.setOnClickListener {
+         startActivity(Intent(this@MainActivity, SampleGlobalLaunch::class.java))
       }
    }
-}
-
-private fun launchGlobalImmediate() {
-   logMsg { "1" }
-   fGlobalLaunch {
-      logMsg { "2" }
-   }
-   logMsg { "3" }
-}
-
-private fun launchGlobalDispatched() {
-   logMsg { "1" }
-   fGlobalLaunch(Dispatchers.Main) {
-      logMsg { "2" }
-   }
-   logMsg { "3" }
 }
 
 inline fun logMsg(block: () -> String) {
---
+++ app/src/main/java/com/sd/demo/coroutines/SampleGlobalLaunch.kt
@@ -0,0 +1,38 @@
+package com.sd.demo.coroutines
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sd.demo.coroutines.databinding.SampleGlobalLaunchBinding
+import com.sd.lib.coroutines.fGlobalLaunch
+import kotlinx.coroutines.Dispatchers
+
+class SampleGlobalLaunch : AppCompatActivity() {
+   private val _binding by lazy { SampleGlobalLaunchBinding.inflate(layoutInflater) }
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(_binding.root)
+      _binding.btnLaunchGlobalImmediate.setOnClickListener {
+         launchGlobalImmediate()
+      }
+      _binding.btnLaunchGlobalDispatched.setOnClickListener {
+         launchGlobalDispatched()
+      }
+   }
+}
+
+private fun launchGlobalImmediate() {
+   logMsg { "1" }
+   fGlobalLaunch {
+      logMsg { "2" }
+   }
+   logMsg { "3" }
+}
+
+private fun launchGlobalDispatched() {
+   logMsg { "1" }
+   fGlobalLaunch(Dispatchers.Main) {
+      logMsg { "2" }
+   }
+   logMsg { "3" }
+}
